6b8417c60acac0b75127dc97efd34fbbf70311f2
Murilo Macedo Silva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2025-04-14 16:39:44 -0300

    .

---
+++ manzano_js/laco_for/js/script.js
@@ -0,0 +1,5 @@
+// exemplo laço for
+
+for(let contadora = 0; contadora < 10; contadora = contadora + 1) {
+    alert(contadora + 'deus!')
+}
---
+++ manzano_js/temporizador/js/script.js
@@ -0,0 +1,9 @@
+// temporizador
+
+for(let hora = 23; hora > -1; hora--) {
+    for(let minuto = 59; minuto > -1; minuto--) {
+        for(let segundo = 59; segundo > -1; segundo--) {
+            console.log(`${hora}:${minuto}:${segundo}`);
+        }
+    }
+}
